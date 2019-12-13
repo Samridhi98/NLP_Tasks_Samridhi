2011495c934967010ec220a9c37c8062fcbf00d2
Samridhi98 <39233679+Samridhi98@users.noreply.github.com>
2019-12-13 13:40:19 +0530

    Add files via upload

---
+++ week1/task1/17BCE2038.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Representing a string
+str = "Shakespeare gave the world best of theatrics"
+print(str)
+
+
+# In[2]:
+
+
+#Using triple single quotes
+str2 = '''William Shakespeare (bapt. 26 April 1564 – 23 April 1616)[a] was an English poet, playwright, and actor, widely regarded as the greatest writer in the English language and the world's greatest dramatist.[2][3][4] He is often called England's national poet and the "Bard of Avon" (or simply "the Bard").[5][b] His extant works, including collaborations, consist of some 39 plays,[c] 154 sonnets, two long narrative poems, and a few other verses, some of uncertain authorship. His plays have been translated into every major living language and are performed more often than those of any other playwright.[7] Shakespeare was born and raised in Stratford-upon-Avon, Warwickshire. At the age of 18, he married Anne Hathaway, with whom he had three children: Susanna and twins Hamnet and Judith. Sometime between 1585 and 1592, he began a successful career in London as an actor, writer, and part-owner of a playing company called the Lord Chamberlain's Men, later known as the King's Men. At age 49 (around 1613), he appears to have retired to Stratford, where he died three years later. Few records of Shakespeare's private life survive; this has stimulated considerable speculation about such matters as his physical appearance, his sexuality, his religious beliefs, and whether the works attributed to him were written by others'''
+#Using triple double quotes
+str3 = """William Shakespeare (bapt. 26 April 1564 – 23 April 1616)[a] was an English poet, playwright, and actor, widely regarded as the greatest writer in the English language and the world's greatest dramatist.[2][3][4] He is often called England's national poet and the "Bard of Avon" (or simply "the Bard").[5][b] His extant works, including collaborations, consist of some 39 plays,[c] 154 sonnets, two long narrative poems, and a few other verses, some of uncertain authorship. His plays have been translated into every major living language and are performed more often than those of any other playwright.[7] Shakespeare was born and raised in Stratford-upon-Avon, Warwickshire. At the age of 18, he married Anne Hathaway, with whom he had three children: Susanna and twins Hamnet and Judith. Sometime between 1585 and 1592, he began a successful career in London as an actor, writer, and part-owner of a playing company called the Lord Chamberlain's Men, later known as the King's Men. At age 49 (around 1613), he appears to have retired to Stratford, where he died three years later. Few records of Shakespeare's private life survive; this has stimulated considerable speculation about such matters as his physical appearance, his sexuality, his religious beliefs, and whether the works attributed to him were written by others"""
+
+print(str2)
+print("===================")
+print(str3)
+
+
+# In[3]:
+
+
+#representing transalation of str1 in hindi
+str4 = "शेक्सपियर ने दुनिया को सर्वश्रेष्ठ नाट्यशास्त्र दिया"
+print(str4)
+
+
+# In[4]:
+
+
+#representing translation of first para of wikipedia
+str5 = '''विलियम शेक्सपियर (बपतिस्मा। 26 अप्रैल 1564 - 23 अप्रैल 1616) [a] एक अंग्रेजी कवि, नाटककार और अभिनेता थे, जिन्हें व्यापक रूप से अंग्रेजी भाषा का सबसे बड़ा लेखक और दुनिया का सबसे बड़ा नाटककार माना जाता था। [२] [३] [४] ] उन्हें अक्सर इंग्लैंड का राष्ट्रीय कवि और "बार्ड ऑफ़ एवन" (या बस "बार्ड") कहा जाता है। [५] [ख] उनके विलुप्त होने वाले कार्यों में सहयोग भी शामिल है, जिसमें ३ ९ नाटक, [ग] १५४ सोननेट, दो लंबे शामिल हैं कथात्मक कविताएँ, और कुछ अन्य छंद, कुछ अनिश्चित लेखक। उनके नाटकों को हर प्रमुख जीवित भाषा में अनुवादित किया गया है और किसी भी अन्य नाटककार की तुलना में अधिक बार प्रदर्शन किया जाता है'''
+print(str5)
+
+
+# In[ ]:
+
+
+
+
